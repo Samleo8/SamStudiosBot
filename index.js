848cdd1f482e1df968370e93d30b877208656d9a
Samuel Leong <leowhiz@yahoo.com.sg>
2019-07-09 19:30:38 +0800

    Why is this not working

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ bot.on('callback_query', (ctx)=>{
 	let urlName = (typeof cb.game_short_name == "undefined")?cb.data:cb.game_short_name;
 
 	let gameURL = getGameURL(urlName);
-	console.log("Returned game url", urlName);
+	console.log("Returned game url", gameURL);
 
 	//WEIRD BUG: ignore whatever is said about using `answerCbQuery` instead; it doesn't work
 	return ctx.answerGameQuery(gameURL);
