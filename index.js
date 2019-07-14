c55db951be4c0bcbfd9cb71659739111bca1d45c
Samuel Leong <leowhiz@yahoo.com.sg>
2019-07-15 00:00:32 +0800

    Stupid comma

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ bot.on('callback_query', (ctx)=>{
 
 	let _data = {
 		"userID": ctx.from.id,
-		"chatID": ctx.chat.id
+		"chatID": ctx.chat.id,
 		"messageID": cb.message.message_id,
 	};
 	//TODO: check if need inline_message_id support (unlikely)
