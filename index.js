122bb5b3dda88467e3b79faa778ac9c7a0fa7bdf
Samuel Leong <leowhiz@yahoo.com.sg>
2020-08-20 16:56:54 +0800

    Attempt at making base url more general

--- index.js
+++ index.js
@@ -93,6 +93,7 @@ bot.command('start', (ctx)=>{
 
 //Get Game URL
 const BASE_URL = "https://samstudiosbot.herokuapp.com";
+console.log(process.env);
 
 let getGameURL = (nm, data) => {
 	let found = validGames.find(el => el === nm);
